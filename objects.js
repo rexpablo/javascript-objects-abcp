9b510d93b85f016a1f1a1424633c3184008dbcf9
Pablo Cuadros <pablocuadrosz115@gmail.com>
2018-01-16 18:31:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,11 +2,11 @@ var playlist = new Object ({artist: 'song'});
 
 
 function updatePlaylist(playlist,artistName,songTitle){
-  playlist[artistName] = songTitle
-  return playlist
+  playlist[artistName] = songTitle;
+  return playlist;
 }
 
 
 function removeFromPlaylist (playlist, artistName){
- return delete playlist.artistName
+ return delete playlist[artistName];
 }
